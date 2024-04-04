d211cf17e1c258e30a5d6b82f4ea7f40697fd25b
PaulDieterich <Paul.dieterich0@gmail.com>
2024-04-04 23:45:45 +0200

    add preparestatement insert parsing

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #include "types/inputBuffer.h"
 
 
+PrepareResult prepare_insert(InputBuffer *buffer, Statement *statement);
+
 /**
  *  read input line and write the input_length from input_buffer
  *  and set buffer from inputBuffer
@@ -64,15 +66,7 @@ MetaCommandResult do_meta_command(InputBuffer* inputBuffer){
 PrepareResult prepare_statement(InputBuffer* inputBuffer, Statement* statement){
 
     if(strncmp(inputBuffer->buffer,"insert",6) == 0){
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-                inputBuffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
-
-        if(args_assigned < 3){
-            return PREPARE_SYNTAX_ERROR;
-        }
-
-        return PREPARE_SUCCESS;
+        return prepare_insert(inputBuffer, statement);
     }
     if(strcmp(inputBuffer->buffer,"select") == 0){
         statement->type = STATEMENT_SELECT;
@@ -81,6 +75,30 @@ PrepareResult prepare_statement(InputBuffer* inputBuffer, Statement* statement){
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
+PrepareResult prepare_insert(InputBuffer *inputBuffer, Statement *statement) {
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword = strtok(inputBuffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok( NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if(id_string == NULL || username == NULL || email == NULL){
+        return PREPARE_SYNTAX_ERROR;
+    }
+    int id = atoi(id_string);
+    if(strlen(username) > COLUMN_USERNAME_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+if(strlen(email) > COLUMN_EMAIL_SIZE){
+    return PREPARE_STRING_TOO_LONG;
+}
+statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username,username);
+    strcpy(statement->row_to_insert.email, email);
+
+return PREPARE_SUCCESS;
+}
 /**
  * Execute statement
  * @param statement
@@ -122,6 +140,9 @@ int main(int argc, char* argv[]){
         switch (prepare_statement(input_buffer, &statement)) {
             case (PREPARE_SUCCESS):
                 break;
+            case(PREPARE_STRING_TOO_LONG):
+                printf("String is too long. \n");
+                continue;
             case (PREPARE_SYNTAX_ERROR):
                 printf("Syntax error. Could not prepare statement: %u. \n", statement.type);
                 continue;
